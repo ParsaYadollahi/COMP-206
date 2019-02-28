e27d9118049b7e734bcfdd80bb07a5d23d98f780
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-02-27 21:42:08 -0500

    Fix ptr issue

--- Assignment/Assignment4/Assignment4.c
+++ Assignment/Assignment4/Assignment4.c
@@ -15,10 +15,10 @@ int main(void)
 {
     int matrix[rows][cols];
     fillMatrix(matrix); // Q1
-    printMatrix(matrix); // Q2
+    // printMatrix(matrix); // Q2
     transposeMatrix(matrix); // Q3
     printMatrix(matrix);
-    int matrix2[2][cols]={{0,1,2,3,4},{5,6,7,8,9}};
+    int matrix2[2][cols]={0,1,2,3,4,5,6,7,8,9};
     int matrixResult[rows][cols];
     multiplyMatrix(matrix2, matrix, matrixResult); // Q4
     printMatrix(matrixResult);
@@ -74,20 +74,20 @@ void transposeMatrix(int matrix[rows][cols]){
 void multiplyMatrix(int m1[2][cols], int m2[rows][cols], int resultMatrix[rows][cols])
 {
     int i, j, k;
-    int *p1, *p2, *p3;
+    int *p1, *p2, p3;
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
             if (i >= 2) {
                 resultMatrix[i][j] = 0;
             } else {
-                *p3 = 0;
+                p3 = 0;
                 for (k = 0; k < cols; k++) {
                     p1 = &m1[i][k];
                     p2 = &m2[k][j];
                     
-                    *p3 += *p1 * *p2;
+                    p3 += *p1 * *p2;
                 }
-                resultMatrix[i][j] = *p3;
+            resultMatrix[i][j] = p3;
             }
         }
     }
