bcc088978951e679f1a6e2ae4d1d83f942d2463c
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-03-05 20:57:10 -0500

    linkedlist

--- Assignment/Assignment5/Assigmnent5.c
+++ Assignment/Assignment5/Assigmnent5.c
@@ -12,6 +12,8 @@ Node* create_dll_from_array(int array[], int size);
 void print_dll(Node* head);
 void insert_after(Node* head, int valueToInsertAfter, int valueToBeInserted);
 void delete_element(Node* head, int valueToBeDeleted);
+void sort_dll(Node* head);
+void swap(Node* prev, Node* current);
 
 
 int main(){
@@ -31,6 +33,9 @@ int main(){
     delete_element(head, 22);
     print_dll(head);
     delete_element(head, 11);
+    print_dll(head);
+    /* Question 5 */
+    sort_dll(head);
     print_dll(head);
     return 0;
 }
@@ -63,7 +68,6 @@ Node* create_dll_from_array(int array[], int size){
 
 void print_dll(Node* head){
     Node* current = head;
-    // printf("%d\n", current->next->data);
     while (current != NULL) {
 
         printf("%d\t", current->data);
@@ -73,7 +77,6 @@ void print_dll(Node* head){
 }
 
 void insert_after(Node* head, int valueToInsertAfter, int valueToBeInserted){
-    // Create a New Node
 
     Node *new_Node;
     Node *current;
@@ -87,18 +90,21 @@ void insert_after(Node* head, int valueToInsertAfter, int valueToBeInserted){
 
     while (current != NULL){
         if (current->data == valueToInsertAfter) {
-
             new_Node->next = current->next;
             new_Node->previous = current->next->previous;
             current->next->previous = new_Node;
             current->next = new_Node;
+            return;
+        } else if (current->data != valueToBeInserted && current->next == NULL){
+            current->next = new_Node;
+            new_Node->previous = current;
+            new_Node->next = NULL;
             return;
         }
         current = current->next;
     }
     free(new_Node);
     free(current);
-    printf("insert case\n");
 }
 
 void delete_element(Node* head, int valueToBeDeleted){
@@ -108,20 +114,15 @@ void delete_element(Node* head, int valueToBeDeleted){
             // First element edge case
             if (head->data == valueToBeDeleted){
                 
-                printf("First Case\n");
 
                 head->data = head->next->data;
                 head->next = head->next->next;
-                current = head->next->next;
-                current->previous = head;
-                // free(current);
-                
+                head->next->next->previous = head;
                 return;
             }
 
             // Not first nor last element
             if (current->data == valueToBeDeleted && current->next != NULL && current->previous !=NULL){
-                printf("second Case\n");
 
                 current->next->previous = current->previous;
                 current->previous->next = current->next;
@@ -131,13 +132,34 @@ void delete_element(Node* head, int valueToBeDeleted){
 
             // Last element edge case
             if (current->data == valueToBeDeleted){
-
-                printf("Third case\n");
+                
                 current->previous->next = NULL;
-
                 free(current);
                 return;
             }
     current = current->next;
     }
+}
+
+void sort_dll(Node* head){
+    Node* current;
+    Node* pointer;
+    pointer = head;
+    while (pointer != NULL){
+        current = head;
+        while (current->next != NULL){
+            if (current->data > current->next->data){
+                swap(current, current->next);
+            }
+            current = current->next;
+        }
+        pointer = pointer->next;
+    }
+}
+
+void swap(Node* prev, Node* current){
+    int temp;
+    temp = current->data;
+    current->data = prev->data;
+    prev->data = temp;   
 }
---
+++ Assignment/Assignment5/tester.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gcc -o Assignment5 Assigmnent5.c 
+chmod u+x tester.sh
+./Assignment5
