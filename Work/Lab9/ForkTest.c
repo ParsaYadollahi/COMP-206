a8ba0228fcebe71e4d4639d34992cdabe519ef99
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-04-05 18:37:02 -0400

    Work of the fork

---
+++ Work/Lab9/ForkTest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int x = 1;
+
+    if (fork() == 0){
+        printf("Child has x = %d\n", ++x);
+    } else {
+        printf("Parent has x = %d\n ", --x);
+    }
+}
